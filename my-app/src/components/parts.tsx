67332871f75954f908bffcc1b9d1d17f3f5bda6d
Is2008 <iergeshew@gmsil.com>
2025-04-08 16:36:27 +0500

    fi

--- my-app/src/components/parts.tsx
+++ my-app/src/components/parts.tsx
@@ -9,8 +9,8 @@ import photo4 from "../assets/10662626w.webp"
 const Parts = () => {
   return (
     <div id="parts">
-        <Linktopart id="1stpart" src={photo1} title="Die Wirkung von Umweltprotesten auf den Klimaschutz"></Linktopart>
-        <Linktopart id="2ndpart"src={photo2} title="Einfluss von Protesten auf die Meinung der Macht "></Linktopart>
+        <Linktopart src={photo1} title="Die Wirkung von Umweltprotesten auf den Klimaschutz"></Linktopart>
+        <Linktopart src={photo2} title="Einfluss von Protesten auf die Meinung der Macht "></Linktopart>
         <Linktopart src={photo3} title="Einfluss von Protesten auf die gesellschaftliche Meinung"></Linktopart>
         <Linktopart src={photo4} title="Die Storüngen und Zerschlagung durch die Protesten"></Linktopart>
     </div>
